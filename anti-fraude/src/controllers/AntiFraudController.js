77d6c36fc3978632a66585bf5234344164cadeb2
juliajeske <julia.jeske@wehandle.com.br>
2023-06-27 18:19:06 -0300

    fix de eslint para todos os sistemas e add de arquivo editorConfig

--- anti-fraude/src/controllers/AntiFraudController.js
+++ anti-fraude/src/controllers/AntiFraudController.js
@@ -10,26 +10,29 @@ class AntiFraudController {
     });
   };
 
-  //TODO finalizar busca por status
+  // TODO finalizar busca por status
   static findAntiFraudByStatus = async (_req, res) => {
-    try {
-      const antiFraud = await AntiFraud.find({ status: 'Em análise' });
-      res.status(200).json(antiFraud);
-    } catch (err) {
-      res.status(500).send({ message: err.message });
-    }
+    AntiFraud.find({ status: 'Em análise' }, (err, foundAntifraud) => {
+      if (err) {
+        return res.status(500).send({ message: err.message });
+      }
+      if (!foundAntifraud) {
+        return res.status(404).json();
+      }
+      return res.status(200).json(foundAntifraud);
+    });
   };
 
   static findAntiFraudById = (req, res) => {
     const { id } = req.params;
-    AntiFraud.findById(id, (err, AntiFraud) => {
+    AntiFraud.findById(id, (err, foundAntifraud) => {
       if (err) {
         return res.status(500).send({ message: err.message });
       }
-      if (!AntiFraud) {
+      if (!foundAntifraud) {
         return res.status(404).json();
       }
-      return res.status(200).json(AntiFraud);
+      return res.status(200).json(foundAntifraud);
     });
   };
 
@@ -37,25 +40,36 @@ class AntiFraudController {
     const myAntiFraud = new AntiFraud({
       ...req.body,
       createdDate: Date(),
-      //TODO ajustar status: 'Em análise'
+      // TODO ajustar status: 'Em análise'
     });
     myAntiFraud.save((err, newAntiFraud) => {
       if (err) {
         return res.status(500).send({ message: err.message });
       }
-      return res.status(201).set('Location', `/antiFraud/${AntiFraud.id}`).json(newAntiFraud);
+      return res
+        .status(201)
+        .set('Location', `/antiFraud/${AntiFraud.id}`)
+        .json(newAntiFraud);
     });
   };
 
   static updateAntiFraud = (req, res) => {
     const { id } = req.params;
 
-    AntiFraud.findByIdAndUpdate(id, { $set: req.body }, { new: true }, (err, AntiFraud) => {
-      if (err) {
-        return res.status(500).send({ message: err.message });
-      }
-      return res.status(204).set('Location', `/antiFraud/${AntiFraud.id}`).send();
-    });
+    AntiFraud.findByIdAndUpdate(
+      id,
+      { $set: req.body },
+      { new: true },
+      (err, foundAntifraud) => {
+        if (err) {
+          return res.status(500).send({ message: err.message });
+        }
+        return res
+          .status(204)
+          .set('Location', `/antiFraud/${foundAntifraud.id}`)
+          .send();
+      },
+    );
   };
 
   static deleteAntiFraud = (req, res) => {
@@ -65,10 +79,11 @@ class AntiFraudController {
       if (err) {
         return res.status(500).send({ message: err.message });
       }
-      return res.status(204).send({ message: 'AntiFraud successfully deleted' });
+      return res
+        .status(204)
+        .send({ message: 'AntiFraud successfully deleted' });
     });
   };
-
 }
 
 export default AntiFraudController;
--- anti-fraude/src/models/AntiFraud.js
+++ anti-fraude/src/models/AntiFraud.js
@@ -1,17 +1,17 @@
 import mongoose from 'mongoose';
 
 const AntiFraudSchema = new mongoose.Schema(
-  
+
   {
     idCliente: {
-      type: Number,//TODO mudar type para mongoose.Schema.Types.ObjectId
-      required: true
+      type: Number, // TODO mudar type para mongoose.Schema.Types.ObjectId
+      required: true,
     },
     idTransação: {
-      type: Number,//TODO mudar type para mongoose.Schema.Types.ObjectId
-      required: true
+      type: Number, // TODO mudar type para mongoose.Schema.Types.ObjectId
+      required: true,
     },
-    status: {type: String, enum: ['Em análise', 'Aprovada', 'Rejeitada']}
+    status: { type: String, enum: ['Em análise', 'Aprovada', 'Rejeitada'] },
   },
 );
 
