9d4063622cb616b75a2848663a1284f2ac890b6d
Marchiori Gabriela Cristina Amancio <n135417@gruposantander.com>
2023-06-29 11:21:45 -0300

    anti-fraude: fix get by Id

--- anti-fraude/src/controllers/AntiFraudController.js
+++ anti-fraude/src/controllers/AntiFraudController.js
@@ -49,7 +49,6 @@ class AntiFraudController {
 
       const clientId = findById.clientId.toString();
       const transactionId = findById.transactionId.toString();
-      console.log(transactionId);
 
       if (!findById) {
         res.status(400).send({ message: 'Anti fraude nao encontrada' });
@@ -57,17 +56,17 @@ class AntiFraudController {
         const responseClient = await fetch(`http://localhost:3001/api/admin/clients/${clientId}`);
         const accounts = await responseClient.json();
 
-        const responseTransacao = await fetch(`http://localhost:3002/api/admin/transactions/${findById.transactionId}`);
+        const responseTransacao = await fetch(`http://localhost:3002/api/admin/transactions/${transactionId}`);
         const transacoes = await responseTransacao.json();
 
         const retorno = {
           _id: findById.id,
           status: findById.status,
           dadosPessoais: {
-            id: accounts.dadosPessoais.id, nome: accounts.dadosPessoais.nome, cpf: accounts.dadosPessoais.cpf, telefone: accounts.dadosPessoais.telefone, renda_mensal: accounts.dadosPessoais.renda_mensal, vencimento_fatura: accounts.cartão.vencimento_fatura,
+            id: accounts.dadosPessoais.id, nome: accounts.dadosPessoais.nome, cpf: accounts.dadosPessoais.cpf, telefone: accounts.dadosPessoais.telefone, rendaMensal: accounts.dadosPessoais.rendaMensal,
           },
           endereco: accounts.endereco,
-          transacao: { _id: transacoes.id, valor: transacoes.valor },
+          transacao: { _id: transacoes._id, valor: transacoes.valor },
         };
 
         res.status(200).json(retorno);
