7893b0cd9c72dc8204cd528905adcd4d4d9d83f4
Tamirys Nogueira <tamirysnogueira22@hotmail.com>
2023-06-30 10:23:30 -0300

    3 - insert authentication middleware

--- clientes/src/routes/accountsRoutes.js
+++ clientes/src/routes/accountsRoutes.js
@@ -1,15 +1,16 @@
 import express from 'express';
-import passport from '../middlware/estrategias.js';
+// import passport from '../middleware/estrategias.js';
 import AccountController from '../controllers/AccountsController.js';
 // import token from '../middlware/tokenteste.js';
+import { bearer, local } from '../middleware/autenticationMiddleware.js';
 
 const router = express.Router();
 
 router
-  .get('/api/admin/accounts', passport.authenticate('bearer', { session: false }), AccountController.findAccounts)
+  .get('/api/admin/accounts', bearer, AccountController.findAccounts)
   .get('/api/admin/accounts/:id', AccountController.findAccountById)
   .post('/api/admin/accounts', AccountController.createAccount)
-  .post('/api/accounts/login', passport.authenticate('local', { session: false }), AccountController.login)
+  .post('/api/accounts/login', local, AccountController.login)
   .put('/api/admin/accounts/:id', AccountController.updateAccount)
   .delete('/api/admin/accounts/:id', AccountController.deleteAccount);
 
