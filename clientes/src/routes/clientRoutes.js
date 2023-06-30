d1952be70ba13ddf02aa6df3a4b71da80c6865c6
NinaS23 <mariabevenuto062@gmail.com>
2023-06-30 11:42:39 -0300

    fix: client route - rm autentication

--- clientes/src/routes/clientRoutes.js
+++ clientes/src/routes/clientRoutes.js
@@ -1,11 +1,10 @@
 import express from 'express';
-import { bearer, local } from '../middleware/autenticationMiddleware.js';
 import ClienteController from '../controllers/ClienteController.js';
 
 const router = express.Router();
 
 router
-  .get('/api/admin/clients/:id', bearer, ClienteController.getUserDataWithoutAccount)
-  .post('/api/admin/clients', local, ClienteController.validDataAtDatabase);
+  .get('/api/admin/clients/:id', ClienteController.getUserDataWithoutAccount)
+  .post('/api/admin/clients', ClienteController.validDataAtDatabase);
 
 export default router;
