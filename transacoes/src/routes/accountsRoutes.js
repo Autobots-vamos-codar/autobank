0d9862787cdfd3169b1e4ca7dda579c4c8d0bf1d
Marchiori Gabriela Cristina Amancio <n135417@gruposantander.com>
2023-06-30 16:27:24 -0300

    transacao: add login

--- transacoes/src/routes/accountsRoutes.js
+++ transacoes/src/routes/accountsRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import TransactionController from '../controllers/TransactionsController.js';
-import { bearer } from '../middleware/autenticationMiddleware.js';
+import { bearer, local } from '../middleware/autenticationMiddleware.js';
+import AccountController from '../controllers/AccountsController.js';
 
 const router = express.Router();
 
 router
   .post('/api/admin/transactions', bearer, TransactionController.createTransaction)
+  .post('/api/accounts/login', local, AccountController.login)
   .get('/api/admin/transactions', bearer, TransactionController.findTransactions)
   .get('/api/admin/transactions/:id', bearer, TransactionController.findTransactionById)
   .put('/api/admin/transactions/:id', TransactionController.updateTransactionStatus);
