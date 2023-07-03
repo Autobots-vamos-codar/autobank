40a53f925597797f9b81162e366c06af2460c35d
Tamirys Nogueira <tamirysnogueira22@hotmail.com>
2023-07-03 09:03:34 -0300

    routes of accounts

--- transacoes/src/routes/accountsRoutes.js
+++ transacoes/src/routes/accountsRoutes.js
@@ -1,11 +1,15 @@
 import express from 'express';
-import { local } from '../middleware/autenticationMiddleware.js';
+import { local, bearer } from '../middleware/autenticationMiddleware.js';
 import AccountController from '../controllers/AccountsController.js';
 
 const router = express.Router();
 
 router
+  .get('/api/admin/accounts', bearer, AccountController.findAccounts)
+  .get('/api/admin/accounts/:id', AccountController.findAccountById)
   .post('/api/admin/accounts', AccountController.createAccount)
-  .post('/api/accounts/login', local, AccountController.login);
+  .post('/api/accounts/login', local, AccountController.login)
+  .put('/api/admin/accounts/:id', AccountController.updateAccount)
+  .delete('/api/admin/accounts/:id', AccountController.deleteAccount);
 
 export default router;
