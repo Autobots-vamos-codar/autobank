a10b756578373af23718b48166a74f59f343ac38
Tamirys Nogueira <tamirysnogueira22@hotmail.com>
2023-07-03 17:36:15 -0300

    fixing bugs of tests

--- transacoes/src/test/TransactionsRoutes.test.js
+++ transacoes/src/test/TransactionsRoutes.test.js
@@ -1,10 +1,40 @@
 import {
-  describe, it, expect,
+  beforeEach, afterEach, afterAll, describe, it, expect,
 } from '@jest/globals';
 import request from 'supertest';
 import app from '../app.js';
 
 let token = '';
+let server;
+
+beforeEach(() => {
+  const port = 3002;
+  server = app.listen(port);
+});
+
+afterEach(() => {
+  server.close();
+});
+
+afterAll((done) => {
+  server.close();
+  done();
+});
+describe('Testes de POST em /api/admin/accounts', () => {
+  it('Deve retornar a conta criada', async () => {
+    await request(app)
+      .post('/api/admin/accounts')
+      .set('Accept', 'application/json')
+      .send({
+        nome: 'admin',
+        email: 'admin2@hotmail',
+        senha: 'admin',
+      })
+      .expect('content-type', /json/)
+      .expect(201);
+  });
+});
+
 describe('Logar na conta', () => {
   it('Deve retornar logar a conta', async () => {
     const loggedAccount = await request(app)
@@ -24,6 +54,7 @@ let createdTransaction;
 
 describe('Testes de POST', () => {
   it('Deve retornar a transação criada e aprovada', async () => {
+    console.log(token);
     createdTransaction = await request(app)
       .post('/api/admin/transactions')
       .set('Accept', 'application/json')
