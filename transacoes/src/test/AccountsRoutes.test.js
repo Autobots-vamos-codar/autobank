b376262856cf4f987f0c78cd9771d2077020b803
Tamirys Nogueira <tamirysnogueira22@hotmail.com>
2023-07-03 17:13:55 -0300

    adding more tests

--- transacoes/src/test/AccountsRoutes.test.js
+++ transacoes/src/test/AccountsRoutes.test.js
@@ -19,9 +19,11 @@ afterAll((done) => {
   server.close();
   done();
 });
+
+let createdAccount;
 describe('Testes de POST em /api/admin/accounts', () => {
   it('Deve retornar a conta criada', async () => {
-    await request(app)
+    createdAccount = await request(app)
       .post('/api/admin/accounts')
       .set('Accept', 'application/json')
       .send({
@@ -31,5 +33,27 @@ describe('Testes de POST em /api/admin/accounts', () => {
       })
       .expect('content-type', /json/)
       .expect(201);
+  });
+});
+describe('Testes de GET em /api/admin/accounts/:id', () => {
+  it('Deve retornar o detalhe da conta', async () => {
+    await request(app)
+      .get(`/api/admin/accounts/${createdAccount.body._id}`)
+      .set('Accept', 'application/json')
+      .expect('content-type', /json/)
+      .expect(200);
+  });
+});
+describe('Testes de PUT em /api/admin/accounts/:id', () => {
+  it('Deve retornar o detalhe da conta', async () => {
+    await request(app)
+      .put(`/api/admin/accounts/${createdAccount.body._id}`)
+      .set('Accept', 'application/json')
+      .send({
+        nome: 'admin22',
+        email: 'admin2@hotmail',
+        senha: 'admin',
+      })
+      .expect(204);
   });
 });
