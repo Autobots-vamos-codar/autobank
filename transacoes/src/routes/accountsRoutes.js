585f0b86172329f63baa285f3e12f23d2981bf13
Luna Matana Fortes <luna.matana@pagonxt.com>
2023-07-03 14:28:50 -0300

    Added ping route

--- transacoes/src/routes/accountsRoutes.js
+++ transacoes/src/routes/accountsRoutes.js
@@ -5,6 +5,7 @@ import AccountController from '../controllers/AccountsController.js';
 const router = express.Router();
 
 router
+  .get('/api/admin/ping', bearer, AccountController.ping)
   .get('/api/admin/accounts', bearer, AccountController.findAccounts)
   .get('/api/admin/accounts/:id', AccountController.findAccountById)
   .post('/api/admin/accounts', AccountController.createAccount)
