33c998e76d645ce5e02c72e774343076dd5eaf64
JessicaMMattos <jessicamartins0563@gmail.com>
2023-07-14 18:31:27 -0300

    25 - Mensageria com RabbitMQ na integração entre os serviços de transações e anti-fraude

--- anti-fraude/src/controllers/AntiFraudController.js
+++ anti-fraude/src/controllers/AntiFraudController.js
@@ -1,20 +1,95 @@
 /* eslint-disable max-len */
-import fetch from 'node-fetch';
-import AntiFraud from '../models/AntiFraud.js';
+import fetch from "node-fetch";
+import AntiFraud from "../models/AntiFraud.js";
+import amqp from "amqplib";
+
+let channel;
+
+async function connectQueue() {
+  try {
+    const connection = await amqp.connect(
+      `amqp://adm:adm@${process.env.RABBIT_HOST || "127.0.0.1"}:5672`
+    );
+    const channel1 = await connection.createChannel();
+
+    await channel1.assertQueue("analisesAntiFraude", { durable: true });
+    console.log("Conexão estabelecida com sucesso.");
+    return channel1;
+  } catch (err) {
+    console.error("Erro ao conectar ao RabbitMQ:", err);
+    throw err;
+  }
+}
+
+channel = await connectQueue();
+
+async function sendQueue(analiseAntiFraude) {
+  channel.sendToQueue(
+    "analisesAntiFraude",
+    Buffer.from(JSON.stringify(analiseAntiFraude)),
+    {
+      persistent: true,
+    }
+  );
+
+  console.log("Análise recebida com sucesso.");
+}
+
+async function consumeQueue(findAntiFraud, status) {
+  await channel.consume(
+    "analisesAntiFraude",
+    async (message) => {
+      try {
+        const options = {
+          method: "PUT",
+        };
+
+        await fetch(
+          `http://${
+            process.env.TRANSACOES_HOST || "127.0.0.1"
+          }:3002/api/admin/transactions/${
+            findAntiFraud.transactionId
+          }?status=${status.toLowerCase()}`,
+          options
+        );
+
+        await AntiFraud.findByIdAndUpdate(
+          findAntiFraud._id,
+          { $set: { status } },
+          { new: true }
+        );
+
+        console.log("Análise processada com sucesso.");
+      } catch (err) {
+        channel.nack(message);
+        throw new Error("Erro ao processar a análise.");
+      }
+    },
+    { noAck: true }
+  );
+}
 
 async function validarTransactionId(idTrasacao) {
-  const responseTransacao = await fetch(`http://${process.env.TRANSACOES_HOST || '127.0.0.1'}:3002/api/admin/transactions/${idTrasacao}`);
+  const responseTransacao = await fetch(
+    `http://${
+      process.env.TRANSACOES_HOST || "127.0.0.1"
+    }:3002/api/admin/transactions/${idTrasacao}`
+  );
 
   if (responseTransacao.status === 404) {
-    throw new Error('Transação não encontrada.');
+    throw new Error("Transação não encontrada.");
   }
 }
 
 async function validarClientId(idCliente) {
-  const responseClient = await fetch(`http://${process.env.CLIENTS_HOST || '127.0.0.1'}:3001/api/admin/clients/${idCliente}`);
+  const responseClient = await fetch(
+    `http://${
+      process.env.CLIENTS_HOST || "127.0.0.1"
+    }:3001/api/admin/clients/${idCliente}`
+  );
 
   if (responseClient.status === 404) {
-    throw new Error('Cliente não encontrado.');
+    throw new Error("Cliente não encontrado.");
   }
 }
 
@@ -22,13 +97,16 @@ async function validarStatus(antiFraud, novoStatus) {
   const statusAtual = antiFraud.status;
 
   if (!antiFraud) {
-    throw new Error('Análise anti-fraude não encontrada.');
+    throw new Error("Análise anti-fraude não encontrada.");
   }
-  if (statusAtual.toLowerCase() === 'aprovada' || statusAtual.toLowerCase() === 'rejeitada') {
-    throw new Error('Status não pode ser alterado.');
+  if (
+    statusAtual.toLowerCase() === "aprovada" ||
+    statusAtual.toLowerCase() === "rejeitada"
+  ) {
+    throw new Error("Status não pode ser alterado.");
   }
-  if (novoStatus !== 'aprovada' && novoStatus !== 'rejeitada') {
-    throw new Error('Status inválido.');
+  if (novoStatus !== "aprovada" && novoStatus !== "rejeitada") {
+    throw new Error("Status inválido.");
   }
 }
 
@@ -44,7 +122,9 @@ class AntiFraudController {
 
   static findAntiFraudUnderReview = async (_req, res) => {
     try {
-      const antiFraudePorStatus = await AntiFraud.find({ status: 'em análise' });
+      const antiFraudePorStatus = await AntiFraud.find({
+        status: "em análise",
+      });
       const data = antiFraudePorStatus.map((item) => ({
         id: item.id,
         clientId: item.clientId,
@@ -67,19 +147,30 @@ class AntiFraudController {
       const transactionId = findById.transactionId.toString();
 
       if (!findById) {
-        res.status(400).send({ message: 'Anti fraude nao encontrada' });
+        res.status(400).send({ message: "Anti fraude nao encontrada" });
       } else {
-        const responseClient = await fetch(`http://${process.env.CLIENTS_HOST || '127.0.0.1'}:3001/api/admin/clients/${clientId}`);
+        const responseClient = await fetch(
+          `http://${
+            process.env.CLIENTS_HOST || "127.0.0.1"
+          }:3001/api/admin/clients/${clientId}`
+        );
         const accounts = await responseClient.json();
-        
-        const responseTransacao = await fetch(`http://${process.env.TRANSACOES_HOST || '127.0.0.1'}:3002/api/admin/transactions/${transactionId}`);
+
+        const responseTransacao = await fetch(
+          `http://${
+            process.env.TRANSACOES_HOST || "127.0.0.1"
+          }:3002/api/admin/transactions/${transactionId}`
+        );
         const transacoes = await responseTransacao.json();
-        
+
         const retorno = {
           _id: findById.id,
           status: findById.status,
           dadosPessoais: {
-            nome: accounts.dadosPessoais.nome, cpf: accounts.dadosPessoais.cpf, telefone: accounts.dadosPessoais.telefone, rendaMensal: accounts.dadosPessoais.rendaMensal,
+            nome: accounts.dadosPessoais.nome,
+            cpf: accounts.dadosPessoais.cpf,
+            telefone: accounts.dadosPessoais.telefone,
+            rendaMensal: accounts.dadosPessoais.rendaMensal,
           },
           Endereco: accounts.Endereco,
           vencimentoDaFatura: accounts.vencimentoDaFatura,
@@ -96,14 +187,18 @@ class AntiFraudController {
 
   static createAntiFraud = async (req, res) => {
     try {
-      const infoAntiFraude = { clientId: req.body.clientId, transactionId: req.body.transactionId, status: 'em análise' };
+      const infoAntiFraude = {
+        clientId: req.body.clientId,
+        transactionId: req.body.transactionId,
+        status: "em análise",
+      };
 
       await validarClientId(req.body.clientId);
       await validarTransactionId(req.body.transactionId);
 
       const novaAntifraude = new AntiFraud(infoAntiFraude);
       await novaAntifraude.save();
-      
+
       res.status(201).json(novaAntifraude);
     } catch (err) {
       res.status(500).send({ message: err.message });
@@ -119,15 +214,10 @@ class AntiFraudController {
 
       await validarStatus(findAntiFraud, status.toLowerCase());
 
-      const options = {
-        method: 'PUT',
-      };
+      await sendQueue(findAntiFraud);
+      await consumeQueue(findAntiFraud, status);
 
-      await fetch(`http://${process.env.TRANSACOES_HOST || '127.0.0.1'}:3002/api/admin/transactions/${findAntiFraud.transactionId}?status=${status.toLowerCase()}`, options);
-
-      const atualizaAntifraude = await AntiFraud.findByIdAndUpdate(id, { $set: { status } }, { new: true });
-
-      res.status(200).json(atualizaAntifraude);
+      res.status(200).json("Status da análise atualizado com sucesso!");
     } catch (err) {
       res.status(500).send({ message: err.message });
     }
@@ -138,9 +228,9 @@ class AntiFraudController {
       const { id } = req.params;
       const deletaAntifraude = await AntiFraud.findByIdAndDelete(id);
       if (!deletaAntifraude) {
-        res.status(404).send({ message: 'anti fraude nao encontrada' });
+        res.status(404).send({ message: "anti fraude nao encontrada" });
       } else {
-        res.status(204).send({ message: 'anti fraude nao encontrada' });
+        res.status(204).send({ message: "anti fraude nao encontrada" });
       }
     } catch (err) {
       res.status(500).send({ message: err.message });
--- transacoes/src/services/TransactionService.js
+++ transacoes/src/services/TransactionService.js
@@ -1,12 +1,80 @@
 // eslint-disable-next-line import/no-relative-packages
-import fetch from 'node-fetch';
-import MongoService from './MongoService.js';
-import Transaction from '../models/transaction.js';
+import fetch from "node-fetch";
+import amqp from "amqplib";
+import MongoService from "./MongoService.js";
+import Transaction from "../models/transaction.js";
+
+let channel;
+
+async function connectQueue() {
+  try {
+    const connection = await amqp.connect(
+      `amqp://adm:adm@${process.env.RABBIT_HOST || "127.0.0.1"}:5672`
+    );
+    const channel1 = await connection.createChannel();
+    console.log(channel1);
+
+    await channel1.assertQueue("transacaoEmAnalise", { durable: true });
+    console.log("Conexão estabelecida com sucesso.");
+    return channel1;
+  } catch (err) {
+    console.error("Erro ao conectar ao RabbitMQ:", err);
+    throw err;
+  }
+}
+
+channel = await connectQueue();
+
+async function sendQueue(analiseAntiFraude) {
+  channel.sendToQueue(
+    "transacaoEmAnalise",
+    Buffer.from(JSON.stringify(analiseAntiFraude)),
+    {
+      persistent: true,
+    }
+  );
+
+  console.log("Transação 'status: em análise' recebida com sucesso.");
+}
+
+async function consumeQueue() {
+  await channel.consume(
+    "transacaoEmAnalise",
+    async (message) => {
+      try {
+        const { clientId, _id } = JSON.parse(message.content);
+
+        const url = `http://${
+          process.env.ANTI_FRAUDE_HOST || "127.0.0.1"
+        }:3000/api/antiFraud`;
+        console.log(url);
+
+        await fetch(url, {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ clientId, transactionId: _id }),
+        });
+
+        console.log("Transação 'status: em análise' processada.");
+      } catch (err) {
+        channel.nack(message);
+        throw new Error("Erro ao processar Transação em análise.");
+      }
+    },
+    { noAck: true }
+  );
+}
 
 function isValidStatusToUpdate(oldStatus, newStatus) {
   console.log(`${oldStatus} - ${newStatus}`);
-  if (oldStatus.toLowerCase() === 'em análise') {
-    if (newStatus.toLowerCase() === 'aprovada' || newStatus.toLowerCase() === 'rejeitada') {
+  if (oldStatus.toLowerCase() === "em análise") {
+    if (
+      newStatus.toLowerCase() === "aprovada" ||
+      newStatus.toLowerCase() === "rejeitada"
+    ) {
       return true;
     }
   }
@@ -19,14 +87,16 @@ class TransactionService {
   static async requireIDAndIncome(datasOfTransaction) {
     console.log(datasOfTransaction);
     try {
-      const url = `http://${process.env.CLIENTS_HOST || '127.0.0.1'}:3001/api/admin/card`;
+      const url = `http://${
+        process.env.CLIENTS_HOST || "127.0.0.1"
+      }:3001/api/admin/card`;
       // console.log(url);
       const reqIDToServiceClient = await fetch(url, {
-        method: 'POST',
+        method: "POST",
         body: JSON.stringify(datasOfTransaction),
         headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
+          Accept: "application/json",
+          "Content-Type": "application/json",
         },
       });
 
@@ -49,17 +119,8 @@ class TransactionService {
 
   static async sendDataToServiceAntFraude(datasOfTransaction) {
     try {
-      const { clientId, _id } = datasOfTransaction;
-      const url = `http://${process.env.ANTI_FRAUDE_HOST || '127.0.0.1'}:3000/api/antiFraud`;
-      console.log(url);
-      await fetch(url, {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ clientId, transactionId: _id }),
-      });
+      const { _id } = datasOfTransaction;
+      await sendQueue(datasOfTransaction);
 
       return { statusResponse: 303, id: _id };
     } catch (error) {
@@ -70,28 +131,31 @@ class TransactionService {
 
   static async processApprovedTransaction(datasTransaction, idUser) {
     const dataTransaction = {
-      status: 'Aprovada',
+      status: "Aprovada",
       valor: datasTransaction.valorTransacao,
       nomeTitular: datasTransaction.nomeTitular,
       clientId: idUser,
     };
     await this.saveTransaction(dataTransaction);
-    return { status: 'Aprovada', idUser, statusResponse: 201 };
+    return { status: "Aprovada", idUser, statusResponse: 201 };
   }
 
   static async processUnderReviewTransaction(datasOfTrasaction, idUser) {
     const dataTransaction = {
-      status: 'Em análise',
+      status: "Em análise",
       valor: datasOfTrasaction.valorTransacao,
       nomeTitular: datasOfTrasaction.nomeTitular,
       clientId: idUser,
     };
     const saved = await this.saveTransaction(dataTransaction);
-    const submitToAntiFraudService = await this.sendDataToServiceAntFraude(saved);
+    const submitToAntiFraudService = await this.sendDataToServiceAntFraude(
+      saved
+    );
 
+    await consumeQueue();
     if (!submitToAntiFraudService.error) {
-      submitToAntiFraudService.status = 'Em análise';
-      submitToAntiFraudService.message = 'Transação executada com sucesso!';
+      submitToAntiFraudService.status = "Em análise";
+      submitToAntiFraudService.message = "Transação executada com sucesso!";
     }
 
     return submitToAntiFraudService;
@@ -99,7 +163,10 @@ class TransactionService {
 
   static async saveTransaction(transactionDoc) {
     try {
-      const newDoc = await Transaction({ ...transactionDoc, createdDate: Date() });
+      const newDoc = await Transaction({
+        ...transactionDoc,
+        createdDate: Date(),
+      });
       console.log(`Transação salva, novo arquivo = ${newDoc}`);
       await newDoc.save();
       return newDoc;
@@ -110,17 +177,25 @@ class TransactionService {
   }
 
   static async validateDataOfTransaction(datasOfTransaction) {
-    console.log('Validando dados da transação');
-    const transactionDataRequired = ['valorTransacao', 'nomeTitular', 'validade', 'numeroCartao', 'cvc'];
+    console.log("Validando dados da transação");
+    const transactionDataRequired = [
+      "valorTransacao",
+      "nomeTitular",
+      "validade",
+      "numeroCartao",
+      "cvc",
+    ];
     const dataTransaction = Object.keys(datasOfTransaction);
-    const hasTheDatas = transactionDataRequired.every((data) => dataTransaction.includes(data));
+    const hasTheDatas = transactionDataRequired.every((data) =>
+      dataTransaction.includes(data)
+    );
 
     console.log(`Antes de validar dados - ${hasTheDatas}`);
     if (!hasTheDatas) {
-      console.log('Erro na validação de dados do cartão');
+      console.log("Erro na validação de dados do cartão");
       return false;
     }
-    console.log('Objeto valido até aqui');
+    console.log("Objeto valido até aqui");
 
     return true;
   }
@@ -128,14 +203,18 @@ class TransactionService {
   static async validateIncome(datasTransaction) {
     try {
       // Esperar time de clientes fazer rota de validação de dados
-      console.log('Buscando dados de usuário');
-      const idUserAndDataIncome = await this.requireIDAndIncome(datasTransaction);
+      console.log("Buscando dados de usuário");
+      const idUserAndDataIncome = await this.requireIDAndIncome(
+        datasTransaction
+      );
       console.log(idUserAndDataIncome);
 
       if (!idUserAndDataIncome.error) {
         const { clientId, statusIncome, income } = idUserAndDataIncome;
 
-        const url = `http://${process.env.CLIENTS_HOST || '127.0.0.1'}:3001/api/admin/clients/${clientId}`;
+        const url = `http://${
+          process.env.CLIENTS_HOST || "127.0.0.1"
+        }:3001/api/admin/clients/${clientId}`;
         console.log(url);
 
         const responseUser = await fetch(url);
@@ -143,8 +222,10 @@ class TransactionService {
 
         console.log(user);
 
-        console.log(`Renda mensal = ${income.$numberDecimal} valor transação ${datasTransaction.valorTransacao}`);
-        return { isValid: statusIncome === 'aprovada', id: clientId };
+        console.log(
+          `Renda mensal = ${income.$numberDecimal} valor transação ${datasTransaction.valorTransacao}`
+        );
+        return { isValid: statusIncome === "aprovada", id: clientId };
       }
 
       throw new Error(idUserAndDataIncome.error);
@@ -154,7 +235,7 @@ class TransactionService {
   }
 
   static async processTransaction(transactionBody) {
-    console.log('Processando transação');
+    console.log("Processando transação");
     const transaction = await this.validateDataOfTransaction(transactionBody);
     if (transaction) {
       const income = await this.validateIncome(transactionBody);
@@ -162,9 +243,15 @@ class TransactionService {
         return { statusResponse: Number(income.error) };
       }
       if (income.isValid) {
-        return await this.processApprovedTransaction(transactionBody, income.id);
+        return await this.processApprovedTransaction(
+          transactionBody,
+          income.id
+        );
       } else {
-        return await this.processUnderReviewTransaction(transactionBody, income.id);
+        return await this.processUnderReviewTransaction(
+          transactionBody,
+          income.id
+        );
       }
     } else {
       return { statusResponse: 400 };
@@ -174,17 +261,21 @@ class TransactionService {
   static async processUpdateStatus(id, newStatus) {
     const transaction = await MongoService.findOne(Transaction, { _id: id });
     if (!transaction) {
-      throw new Error('NotFound');
+      throw new Error("NotFound");
     }
     console.log(transaction);
     if (isValidStatusToUpdate(transaction.status, newStatus)) {
-      const doc = await MongoService.updateOne(Transaction, id, { status: newStatus });
+      const doc = await MongoService.updateOne(Transaction, id, {
+        status: newStatus,
+      });
       console.log(doc);
       return;
     }
 
-    throw new Error('Status inválido para atualização. O status atual não permite '
-    + 'atualizações ou o status enviado é inválido.');
+    throw new Error(
+      "Status inválido para atualização. O status atual não permite " +
+        "atualizações ou o status enviado é inválido."
+    );
   }
 }
 
